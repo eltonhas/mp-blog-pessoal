7ec5f22beed168205600b24123e1cbc009d7dc79
eltonhas <elton_has@hotmail.com>
2024-08-29 19:35:27 -0300

    remove prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <header className="font-alt flex w-full items-center justify-center bg-slate-100 py-10">
+    <header className="flex w-full items-center justify-center bg-slate-100 py-10 font-alt">
       <Link href={'/'}>
         <h1 className="text-3xl font-semibold md:text-5xl">
           <span className="text-blue-500">Tech</span>Blog
